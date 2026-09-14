d25c5dcf0454e47e20d5f9c3ece00f23d9d3553c
houssam amimeur <hamimeur@mymail.aacc.edu>
2026-09-14 10:17:07 -0400

    created netmiko connection to cisco device to obtain interface and version information for loopback connections.

---
+++ cisco_interface_check.py
@@ -0,0 +1,30 @@
+from netmiko import ConnectHandler
+
+device = {
+    "device_type": "cisco_ios",
+    "host": "54.90.112.247",
+    "username": "hamimeur",
+    "password": "password101!"
+}
+
+connection = ConnectHandler(**device)
+
+interface_output = connection.send_command("show ip interface brief")
+
+version_output = connection.send_command("show version")
+interface_lines = interface_output.splitlines()
+
+for line in interface_lines:
+    if "Loopback" in line:
+        fields = line.split()
+
+        interface_name = fields[0]
+        ip_address = fields[1]
+        interface_status = fields[4]
+        protocol_status = fields[5]
+
+        if interface_status == "up" and protocol_status == "up":
+            print(f"{interface_name}: OPERATIONAL")
+        else:
+            print(f"{interface_name}: CHECK REQUIRED")
+connection.disconnect()
